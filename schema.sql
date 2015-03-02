8e9aa0ad2f6340e771a676944a486c99251067d2
Sk8rSeth <sethhowell2197@gmail.com>
2015-03-02 12:05:10 -0700

    fixed syntax

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE user (
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     username VARCHAR(255),
-    password VARCHAR(255)
+    password VARCHAR(255),
     email VARCHAR(255),
     gender VARCHAR(50),
     image_id INT
