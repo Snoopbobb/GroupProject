6a25bc8105b83117004654ef2f0685e5209924dc
Sk8rSeth <sethhowell2197@gmail.com>
2015-03-05 14:42:36 -0700

    mountain page

--- GroupProject/app/Http/Controllers/MountainController.php
+++ GroupProject/app/Http/Controllers/MountainController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 use DB;
 use App\Models\Mountain;
+use App\Models\Trail;
 
 class MountainController extends Controller {
 
@@ -14,8 +15,9 @@ class MountainController extends Controller {
 		switch ($mountain_id) {
 
 			case "1":
-				$mountain = new Mountain(1);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;
 			case "2":
 				$mountain = new Mountain(2);
@@ -23,32 +25,39 @@ class MountainController extends Controller {
 				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;
 			case "3":
-				$mountain = new Mountain(3);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;
 			case "4":
-				$mountain = new Mountain(4);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;			
 			case "5":
-				$mountain = new Mountain(5);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;			
 			case "6":
-				$mountain = new Mountain(6);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;			
 			case "7":
-				$mountain = new Mountain(7);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;
 			case "8":
-				$mountain = new Mountain(8);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;
 			case "9":
-				$mountain = new Mountain(9);
-				return view('Mountain', ['mountain' => $mountain]);
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
 			break;
 
 			default:
