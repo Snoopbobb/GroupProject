ec2781152c79ebf5b36372d09c0daab02051c835
Sk8rSeth <sethhowell2197@gmail.com>
2015-03-05 14:31:12 -0700

    changes to logic and mountain pages

--- GroupProject/app/Http/Controllers/MountainController.php
+++ GroupProject/app/Http/Controllers/MountainController.php
@@ -1,10 +1,59 @@
 <?php namespace App\Http\Controllers;
 use DB;
+use App\Models\Mountain;
 
 class MountainController extends Controller {
 
 	public function getAll() {
-		return view('Mountain');
+		return view('Mountain', ['title' => 'All The Mountains', 'description' => 'some random Mountains yo']);
 	}
 
+	public function getMountain($mountain_id) {
+
+
+		switch ($mountain_id) {
+
+			case "1":
+				$mountain = new Mountain(1);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;
+			case "2":
+				$mountain = new Mountain(2);
+				$trail = new Trail(2);
+				return view('Mountain', ['mountain' => $mountain, 'trail' => $trail]);
+			break;
+			case "3":
+				$mountain = new Mountain(3);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;
+			case "4":
+				$mountain = new Mountain(4);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;			
+			case "5":
+				$mountain = new Mountain(5);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;			
+			case "6":
+				$mountain = new Mountain(6);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;			
+			case "7":
+				$mountain = new Mountain(7);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;
+			case "8":
+				$mountain = new Mountain(8);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;
+			case "9":
+				$mountain = new Mountain(9);
+				return view('Mountain', ['mountain' => $mountain]);
+			break;
+
+			default:
+				return view('Mountains/all');
+			break;
+		}
+	}
 }
--- GroupProject/resources/views/Mountain.blade.php
+++ GroupProject/resources/views/Mountain.blade.php
@@ -1,1 +1,38 @@
 @extends('layout')
+
+@section('title')
+<h1>{{ $mountain->name }}</h1>
+<div>** weather **</div>
+<div>** random stuff **</div>
+@stop
+
+
+@section('tagline')
+<div class="tagline">
+	<h2>{{ $mountain->description }}</h2>
+	<div>
+		<span class="selected">Top 10</span>
+		<span>See All</span>
+	</div>
+</div>
+@stop
+
+
+@section('main_content')
+	<div class="trail_tiles">
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_1"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_2"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_3"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_4"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_5"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_6"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_7"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_8"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_9"></div></a>
+		<a href="/Trails/{{ trail->trail_id }}"><div class="trail_tile_10"></div></a>
+	</div>
+	<div class="seeAll">
+		{{  }}
+	</div>
+
+@stop
