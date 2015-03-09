a6024f40b48c20341ec32edc4689d74d6a398848
Robert Tabor <bobtabor@q.com>
2015-03-09 16:33:31 -0700

    started suggest page

--- GroupProject/resources/views/pages/suggest.blade.php
+++ GroupProject/resources/views/pages/suggest.blade.php
@@ -7,17 +7,39 @@
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
 		<div class="form-group">
 			Mountain Name:
-			<input type="text" class="form-control" name="mountain_name">
+			<input type="text" class="suggest-input" name="mountain_name">
 		</div>
 		<div class="form-group">
 			Trail Name:
-			<input type="text" class="form-control" name="trail_name">
+			<input type="text" class="suggest-input" name="trail_name">
 		</div>
 		<div class="form-group">
 			Trail Location:
-			<input type="text" class="form-control" name="trail_location">
+			<input type="text" class="suggest-input" name="trail_location">
 		</div>
-		<button class="btn btn-group">Submit</button>
+		<div class="form-group">
+			<label>1-3 Miles
+			<input type="radio" name="trail_length" value="1-3_miles">
+			</label>
+		</div>
+		<div class="form-group">
+			<label>3-7 Miles
+			<input type="radio" name="trail_length" value="3-7_miles">
+			</label>
+		</div>
+		<div class="form-group">
+			<label>7-12 Miles
+			<input type="radio" name="trail_length" value="7-12_miles">
+			</label>
+		</div>
+		<div class="form-group">
+			<label>13+ Miles
+			<input type="radio" name="trail_length" value="13+_miles">
+			</label>
+		</div>
+		<div>
+			<button class="btn btn-group">Submit</button>
+		</div>
 	</form>
 </div>
 @stop
