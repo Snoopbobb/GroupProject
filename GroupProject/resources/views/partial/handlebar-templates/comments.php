c12aecac4360dcf26583559625d38ff0a71aca54
Jordyn Tacoronte <jordyntacoronte@gmail.com>
2015-03-09 14:39:32 -0700

    added js

---
+++ GroupProject/resources/views/partial/handlebar-templates/comments.php
@@ -0,0 +1,9 @@
+<script id="template-comment" type="text/x-handlebars-template">
+		<div class="comment-block">
+				<div class="image">this is an image</div>
+				<div class="comment">
+					<div class="user-name">Jordyn</div>
+					<div class="comment-content">{{message}}</div>
+				</div>
+			</div>
+	</script>
