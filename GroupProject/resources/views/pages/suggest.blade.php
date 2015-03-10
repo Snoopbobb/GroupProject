c0826939b13bb459fa345107ba447159521e35f5
Robert Tabor <bobtabor@q.com>
2015-03-10 10:30:08 -0700

    suggest page

--- GroupProject/resources/views/pages/suggest.blade.php
+++ GroupProject/resources/views/pages/suggest.blade.php
@@ -3,42 +3,50 @@
 @section('main_content')
 <div class="suggest-wrapper">
 	<h2>Don't see a trail you love?</h2>
-	<form class="">
+	<h4>Suggest it to us. We'll do some research, and get it on our website as soon as possible.</h4>
+	<form class="suggest-form">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
-		<div class="form-group">
+		<div class="suggest-input">
 			Mountain Name:
 			<input type="text" class="suggest-input" name="mountain_name">
 		</div>
-		<div class="form-group">
+		<div class="suggest-input">
 			Trail Name:
 			<input type="text" class="suggest-input" name="trail_name">
 		</div>
-		<div class="form-group">
+		<div class="suggest-input">
 			Trail Location:
 			<input type="text" class="suggest-input" name="trail_location">
 		</div>
-		<div class="form-group">
-			<label>1-3 Miles
-			<input type="radio" name="trail_length" value="1-3_miles">
-			</label>
-		</div>
-		<div class="form-group">
-			<label>3-7 Miles
-			<input type="radio" name="trail_length" value="3-7_miles">
-			</label>
-		</div>
-		<div class="form-group">
-			<label>7-12 Miles
-			<input type="radio" name="trail_length" value="7-12_miles">
-			</label>
-		</div>
-		<div class="form-group">
-			<label>13+ Miles
-			<input type="radio" name="trail_length" value="13+_miles">
-			</label>
-		</div>
-		<div>
-			<button class="btn btn-group">Submit</button>
+		<div class="suggest-radio-wrapper">
+				Trail Length:
+			<div class="suggest-radio">
+				<label>
+				<input type="radio" name="trail_length" value="1-3_miles">
+				1-3 Miles
+				</label>
+			</div>
+			<div class="suggest-radio">
+				<label>
+				<input type="radio" name="trail_length" value="3-7_miles">
+				3-7 Miles
+				</label>
+			</div>
+			<div class="suggest-radio">
+				<label>
+				<input type="radio" name="trail_length" value="7-12_miles">
+				7-12 Miles
+				</label>
+			</div>
+			<div class="suggest-radio">
+				<label>
+				<input type="radio" name="trail_length" value="13+_miles">
+				13+ Miles
+				</label>
+			</div>
+			<div>
+				<button class="suggest-button">Submit</button>
+			</div>
 		</div>
 	</form>
 </div>
