82fc5266fd370bcbad210bb3b72786791f563ced
Scott Kessinger <scottkessinger@hotmail.com>
2015-03-11 11:51:40 -0700

    update

--- GroupProject/app/Models/Profile.php
+++ GroupProject/app/Models/Profile.php
@@ -5,7 +5,38 @@ use DB;
 use App\Library\Sql;
 use App\Models\Model;
 
-class Image extends Model {
+class Profile extends Model {
 	protected static $table = 'user';
 	protected static $key = 'user_id';
+
+	public static function edit($first_name, 
+								$last_name, 
+								$email, 
+								$gender, 
+								$user_id
+								) {
+		$sql= "UPDATE user
+				SET first_name = :first_name,
+					last_name = :last_name,
+					email = :email,
+					gender = :gender
+				WHERE 
+					user_id = :user_id
+				";
+
+		$update_profile = DB::statement($sql, [':first_name'=>$first_name,
+								':last_name'=>$last_name,
+								':email'=>$email,
+								':gender'=>$gender,
+								':user_id'=>$user_id]);
+
+		return $update_profile;
+	}
+
+	public static function delete($user_id) {
+		$sql="DELETE FROM user WHERE user_id = $user_id";
+		$delete_user = DB::delete($sql);
+
+		return $delete_user;
+	}
 }
