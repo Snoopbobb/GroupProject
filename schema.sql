b92ee1bdccaeb1fdfb666016b87254755b9f211b
Scott Kessinger <scottkessinger@hotmail.com>
2015-03-12 13:15:02 -0700

    update

--- schema.sql
+++ schema.sql
@@ -143,6 +143,7 @@ INSERT INTO image (image_id, image_path) VALUES (19, '/css/images/DreamyDraw/Nat
 INSERT INTO image (image_id, image_path) VALUES (23, '/css/images/LookoutMountain/circumference_and_summer_trail.jpg');
 INSERT INTO image (image_id, image_path) VALUES (24, '/css/images/NorthMountain/northMountainNationalTrail.jpg');
 INSERT INTO image (image_id, image_path) VALUES (20, '/css/images/DreamyDraw/perlCharlesMemorialTrail.jpg');
+INSERT INTO image (image_id, image_path) VALUES (25, '/css/images/ShadowMountain/big-loop.jpg');
 
 -- comments
 INSERT INTO comment(trail_id, user_id, comment_description) VALUES (1, 4, "this trail was ok. I have seen better.");
